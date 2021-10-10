8484c64d386bc262bec9b7b69da5ab437c32d777
koushik173 <cse_2012020173@lus.ac.bd>
2021-10-10 13:58:34 +0600

    initial text

---
+++ src/return_value/ReturningValue.java
@@ -0,0 +1,9 @@
+
+package return_value;
+
+public class ReturningValue {
+    
+    int square(int value){
+        return value*value;
+    }
+}
---
+++ src/return_value/Test.java
@@ -0,0 +1,11 @@
+
+package return_value;
+
+public class Test {
+    public static void main(String[] args) {
+        ReturningValue ob1 = new ReturningValue();
+        
+        System.out.println("Square of 56 = "+ob1.square(56));
+        
+    }
+}
